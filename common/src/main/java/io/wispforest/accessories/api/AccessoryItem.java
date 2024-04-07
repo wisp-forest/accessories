5a574f0e8b3a0e675e19450b5322b101802aba51
Dragon-Seeker <timekeeperguild@gmail.com>
2024-04-07 17:48:11 -0500

    Add AccessoryItem and add maven publishing code

---
+++ common/src/main/java/io/wispforest/accessories/api/AccessoryItem.java
@@ -0,0 +1,12 @@
+package io.wispforest.accessories.api;
+
+import net.minecraft.world.item.Item;
+
+public class AccessoryItem extends Item implements Accessory{
+
+    public AccessoryItem(Properties properties) {
+        super(properties);
+
+        AccessoriesAPI.registerAccessory(this, this);
+    }
+}
