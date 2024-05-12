799e72b6b0046615f1d6180a054164c68e74b59d
Dragon-Seeker <timekeeperguild@gmail.com>
2024-05-11 23:11:16 -0500

    Fix bug with applying slots that don't exist to given entity types

--- tclayer/src/main/java/io/wispforest/tclayer/mixin/EntitySlotLoaderMixin.java
+++ tclayer/src/main/java/io/wispforest/tclayer/mixin/EntitySlotLoaderMixin.java
@@ -2,6 +2,7 @@ package io.wispforest.tclayer.mixin;
 
 import com.google.gson.JsonObject;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.logging.LogUtils;
 import dev.emi.trinkets.compat.WrappingTrinketsUtils;
 import io.wispforest.accessories.api.slot.SlotType;
 import io.wispforest.accessories.data.EntitySlotLoader;
@@ -10,7 +11,10 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.entity.EntityType;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,10 +22,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.HashMap;
 import java.util.Map;
 
+@Debug(export = true)
 @Mixin(EntitySlotLoader.class)
 public abstract class EntitySlotLoaderMixin {
 
-    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap;copyOf(Ljava/util/Map;)Lcom/google/common/collect/ImmutableMap;"), remap = false)
+    @Unique
+    private static final Logger TRINKET_LOGGER = LogUtils.getLogger();
+
+    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap;copyOf(Ljava/util/Map;)Lcom/google/common/collect/ImmutableMap;"))
     private void injectTrinketSpecificSlots(Map<ResourceLocation, JsonObject> data, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci, @Local(name = "tempMap") HashMap<EntityType<?>, Map<String, SlotType>> tempMap){
         var loader = dev.emi.trinkets.data.EntitySlotLoader.SERVER;
 
@@ -33,9 +41,17 @@ public abstract class EntitySlotLoaderMixin {
             for (String s : entry.getValue()) {
                 var convertedType = WrappingTrinketsUtils.trinketsToAccessories_Slot(s);
 
-                if (innerMap.containsKey(convertedType)) continue;
+                if (innerMap.containsKey(convertedType)) {
+                    continue;
+                }
 
                 var slotType = slotTypes.get(convertedType);
+
+                if (slotType == null) {
+                    TRINKET_LOGGER.warn("Unable to locate the given slot for a given entity binding, such will be skipped: [Name: {}]", convertedType);
+
+                    continue;
+                }
 
                 innerMap.put(slotType.name(), slotType);
             }
