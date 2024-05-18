b40e34c15e22a6624abd4a14d783d2dd5c12a9db
Dragon-Seeker <timekeeperguild@gmail.com>
2024-05-18 01:20:06 -0500

    Fix issues with reading and write trinkets component

--- tclayer/src/main/java/dev/emi/trinkets/compat/WrappedTrinketComponent.java
+++ tclayer/src/main/java/dev/emi/trinkets/compat/WrappedTrinketComponent.java
@@ -141,69 +141,81 @@ public abstract class WrappedTrinketComponent implements TrinketComponent {
 
     @Override
     public void readFromNbt(CompoundTag tag) {
-        System.out.println(tag.toString());
+        if(tag.getBoolean("is_accessories_data")) {
+            ((AccessoriesHolderImpl)this.capability.getHolder())
+                    .read(tag.getCompound("main_data"));
+        } else {
+            var dropped = new ArrayList<ItemStack>();
 
-        var dropped = new ArrayList<ItemStack>();
+            for (var groupKey : tag.getAllKeys()) {
+                var groupTag = tag.getCompound(groupKey);
 
-        for (var groupKey : tag.getAllKeys()) {
-            var groupTag = tag.getCompound(groupKey);
+                for (var slotKey : groupTag.getAllKeys()) {
+                    var slotTag = groupTag.getCompound(slotKey);
 
-            for (var slotKey : groupTag.getAllKeys()) {
-                var slotTag = groupTag.getCompound(slotKey);
+                    var slotName = WrappingTrinketsUtils.trinketsToAccessories_Slot(slotKey);
 
-                var slotName = WrappingTrinketsUtils.trinketsToAccessories_Slot(slotKey);
+                    var slotType = SlotTypeLoader.getSlotType(this.getEntity().level(), slotName);
 
-                var slotType = SlotTypeLoader.getSlotType(this.getEntity().level(), slotName);
+                    var list = slotTag.getList("Items", NbtType.COMPOUND)
+                            .stream()
+                            .map(tagEntry -> ItemStack.of((tagEntry instanceof CompoundTag compoundTag) ? compoundTag : new CompoundTag()))
+                            .toList();
 
-                var list = slotTag.getList("Items", NbtType.COMPOUND)
-                        .stream()
-                        .map(tagEntry -> ItemStack.of((tagEntry instanceof CompoundTag compoundTag) ? compoundTag : new CompoundTag()))
-                        .toList();
+                    if (slotType == null) {
+                        dropped.addAll(list);
 
-                if(slotType == null) {
-                    dropped.addAll(list);
-
-                    continue;
-                }
-
-                var container = this.capability.getContainer(slotType);
-
-                if(container == null) {
-                    dropped.addAll(list);
-
-                    System.out.println("Unable to handle the given slotType as a container did not exist");
-
-                    continue;
-                }
-
-                var accessories = container.getAccessories();
-
-                for (var stack : list) {
-                    boolean consumedStack = false;
-
-                    for (int i = 0; i < accessories.getContainerSize() && !consumedStack; i++) {
-                        var currentStack = accessories.getItem(i);
-
-                        if(!currentStack.isEmpty()) continue;
-
-                        var ref = new io.wispforest.accessories.api.slot.SlotReference(slotName, this.getEntity(), i);
-
-                        if(!AccessoriesAPI.canInsertIntoSlot(stack, ref)) continue;
-
-                        accessories.setItem(i, stack.copy());
-
-                        consumedStack = true;
+                        continue;
                     }
 
-                    if(!consumedStack) {
-                        dropped.add(stack.copy());
+                    var container = this.capability.getContainer(slotType);
+
+                    if (container == null) {
+                        dropped.addAll(list);
+
+                        System.out.println("Unable to handle the given slotType as a container did not exist");
+
+                        continue;
+                    }
+
+                    var accessories = container.getAccessories();
+
+                    for (var stack : list) {
+                        boolean consumedStack = false;
+
+                        for (int i = 0; i < accessories.getContainerSize() && !consumedStack; i++) {
+                            var currentStack = accessories.getItem(i);
+
+                            if (!currentStack.isEmpty()) continue;
+
+                            var ref = new io.wispforest.accessories.api.slot.SlotReference(slotName, this.getEntity(), i);
+
+                            if (!AccessoriesAPI.canInsertIntoSlot(stack, ref)) continue;
+
+                            accessories.setItem(i, stack.copy());
+
+                            consumedStack = true;
+                        }
+
+                        if (!consumedStack) {
+                            dropped.add(stack.copy());
+                        }
                     }
                 }
             }
-        }
 
-        ((AccessoriesHolderImpl) this.capability.getHolder()).invalidStacks.addAll(dropped);
+            ((AccessoriesHolderImpl) this.capability.getHolder()).invalidStacks.addAll(dropped);
+        }
     }
 
-    @Override public void writeToNbt(CompoundTag tag) {}
+    @Override
+    public void writeToNbt(CompoundTag tag) {
+        var innerCompound = new CompoundTag();
+
+        ((AccessoriesHolderImpl)this.capability.getHolder())
+                .write(innerCompound);
+
+        tag.put("main_data", innerCompound);
+        tag.putBoolean("is_accessories_data", true);
+    }
 }
