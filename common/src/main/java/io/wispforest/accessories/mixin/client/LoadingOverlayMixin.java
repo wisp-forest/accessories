c6e27e90d642f6b474772430a1287d834149fc23
Dragon-Seeker <timekeeperguild@gmail.com>
2024-07-26 18:29:52 -0500

    Use `Minecraft.getInstance()` instead of shadowing field combined with remap false for ForgeLoadingOverlay

--- common/src/main/java/io/wispforest/accessories/mixin/client/LoadingOverlayMixin.java
+++ common/src/main/java/io/wispforest/accessories/mixin/client/LoadingOverlayMixin.java
@@ -15,12 +15,10 @@ import java.util.function.Consumer;
 @Mixin(LoadingOverlay.class)
 public abstract class LoadingOverlayMixin {
 
-    @Shadow @Final private Minecraft minecraft;
-
     @ModifyVariable(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Overlay;<init>()V", shift = At.Shift.AFTER), argsOnly = true)
     private Consumer<Optional<Throwable>> addEventHook(Consumer<Optional<Throwable>> value) {
         return (throwable) -> {
-            ClientLifecycleEvents.END_DATA_PACK_RELOAD.invoker().endDataPackReload(this.minecraft, throwable.isEmpty());
+            ClientLifecycleEvents.END_DATA_PACK_RELOAD.invoker().endDataPackReload(Minecraft.getInstance(), throwable.isEmpty());
 
             value.accept(throwable);
         };
--- neoforge/src/main/java/io/wispforest/accessories/neoforge/mixin/NeoForgeLoadingOverlayMixin.java
+++ neoforge/src/main/java/io/wispforest/accessories/neoforge/mixin/NeoForgeLoadingOverlayMixin.java
@@ -2,8 +2,7 @@ package io.wispforest.accessories.neoforge.mixin;
 
 import io.wispforest.accessories.client.ClientLifecycleEvents;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.LoadingOverlay;
-import net.neoforged.neoforge.client.loading.NeoForgeLoadingOverlay;
+import net.minecraftforge.client.loading.ForgeLoadingOverlay;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,15 +12,16 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import java.util.Optional;
 import java.util.function.Consumer;
 
-@Mixin(NeoForgeLoadingOverlay.class)
+@Mixin(value = NeoForgeLoadingOverlay.class, remap = false)
 public abstract class NeoForgeLoadingOverlayMixin {
 
-    @Shadow @Final private Minecraft minecraft;
-
-    @ModifyVariable(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/LoadingOverlay;<init>(Lnet/minecraft/client/Minecraft;Lnet/minecraft/server/packs/resources/ReloadInstance;Ljava/util/function/Consumer;Z)V", shift = At.Shift.AFTER), argsOnly = true)
+    @ModifyVariable(
+            method = "<init>",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/LoadingOverlay;<init>(Lnet/minecraft/client/Minecraft;Lnet/minecraft/server/packs/resources/ReloadInstance;Ljava/util/function/Consumer;Z)V", shift = At.Shift.AFTER),
+            argsOnly = true, remap = false)
     private Consumer<Optional<Throwable>> addEventHook(Consumer<Optional<Throwable>> value) {
         return (throwable) -> {
-            ClientLifecycleEvents.END_DATA_PACK_RELOAD.invoker().endDataPackReload(this.minecraft, throwable.isEmpty());
+            ClientLifecycleEvents.END_DATA_PACK_RELOAD.invoker().endDataPackReload(Minecraft.getInstance(), throwable.isEmpty());
 
             value.accept(throwable);
         };
