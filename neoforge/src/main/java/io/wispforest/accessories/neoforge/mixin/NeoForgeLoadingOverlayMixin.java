5d8e7404a25c72bf5c59bacede023bf50dd63d48
Dragon-Seeker <timekeeperguild@gmail.com>
2024-07-26 18:29:52 -0500

    Use `Minecraft.getInstance()` instead of shadowing field combined with remap false for ForgeLoadingOverlay

--- neoforge/src/main/java/io/wispforest/accessories/neoforge/mixin/NeoForgeLoadingOverlayMixin.java
+++ neoforge/src/main/java/io/wispforest/accessories/neoforge/mixin/NeoForgeLoadingOverlayMixin.java
@@ -12,15 +12,16 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import java.util.Optional;
 import java.util.function.Consumer;
 
-@Mixin(ForgeLoadingOverlay.class)
+@Mixin(value = ForgeLoadingOverlay.class, remap = false)
 public abstract class NeoForgeLoadingOverlayMixin {
 
-    @Shadow @Final private Minecraft minecraft;
-
-    @ModifyVariable(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/LoadingOverlay;<init>(Lnet/minecraft/client/Minecraft;Lnet/minecraft/server/packs/resources/ReloadInstance;Ljava/util/function/Consumer;Z)V", shift = At.Shift.AFTER), argsOnly = true)
+    @ModifyVariable(
+            method = "<init>",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/LoadingOverlay;<init>(Lnet/minecraft/client/Minecraft;Lnet/minecraft/server/packs/resources/ReloadInstance;Ljava/util/function/Consumer;Z)V", shift = At.Shift.AFTER),
+            argsOnly = true, remap = false)
     private Consumer<Optional<Throwable>> addEventHook(Consumer<Optional<Throwable>> value) {
         return (throwable) -> {
-            ClientLifecycleEvents.END_DATA_PACK_RELOAD.invoker().endDataPackReload(this.minecraft, throwable.isEmpty());
+            ClientLifecycleEvents.END_DATA_PACK_RELOAD.invoker().endDataPackReload(Minecraft.getInstance(), throwable.isEmpty());
 
             value.accept(throwable);
         };
