38f5b837731fdf37cbc2108a8911b80657f528d1
Dragon-Seeker <timekeeperguild@gmail.com>
2024-07-26 20:05:48 -0500

    Convert slot type within Curios toggle packet

--- cclayer/src/main/java/top/theillusivec4/curios/common/network/client/CPacketToggleRender.java
+++ cclayer/src/main/java/top/theillusivec4/curios/common/network/client/CPacketToggleRender.java
@@ -3,10 +3,11 @@ package top.theillusivec4.curios.common.network.client;
 import io.wispforest.accessories.data.SlotTypeLoader;
 import io.wispforest.accessories.networking.server.SyncCosmeticToggle;
 import io.wispforest.cclayer.WrappedAccessoriesPacket;
+import top.theillusivec4.curios.compat.CuriosWrappingUtils;
 
 public class CPacketToggleRender extends WrappedAccessoriesPacket {
 
     public CPacketToggleRender(String id, int index) {
-        super(new SyncCosmeticToggle(null, SlotTypeLoader.INSTANCE.getSlotTypes(true).get(id).name(), index));
+        super(new SyncCosmeticToggle(null, SlotTypeLoader.INSTANCE.getSlotTypes(true).get(CuriosWrappingUtils.curiosToAccessories(id)).name(), index));
     }
 }
