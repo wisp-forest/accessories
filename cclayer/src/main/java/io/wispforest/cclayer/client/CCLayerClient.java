8d20bf61a3622de75cf5260ad9c41eb6bad27629
Dragon-Seeker <timekeeperguild@gmail.com>
2024-07-27 19:24:11 -0500

    Implement IconHelper instance on client init

--- cclayer/src/main/java/io/wispforest/cclayer/client/CCLayerClient.java
+++ cclayer/src/main/java/io/wispforest/cclayer/client/CCLayerClient.java
@@ -1,6 +1,10 @@
 package io.wispforest.cclayer.client;
 
+import io.wispforest.accessories.api.slot.SlotType;
+import io.wispforest.accessories.data.SlotTypeLoader;
 import io.wispforest.cclayer.CCLayer;
+
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModLoadingContext;
@@ -9,7 +13,10 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.ClientNeoForgeMod;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
+import top.theillusivec4.curios.api.type.util.IIconHelper;
+import top.theillusivec4.curios.compat.CuriosWrappingUtils;
 
 @EventBusSubscriber(modid = CCLayer.MODID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public class CCLayerClient {
@@ -19,6 +26,17 @@ public class CCLayerClient {
         var bus = ModLoadingContext.get().getActiveContainer().getEventBus();
 
         bus.addListener(CCLayerClient::loaderRenders);
+        CuriosApi.setIconHelper(new IIconHelper() {
+            @Override public void clearIcons() {}
+            @Override public void addIcon(String identifier, ResourceLocation resourceLocation) {}
+
+            @Override
+            public ResourceLocation getIcon(String identifier) {
+                var slot = SlotTypeLoader.INSTANCE.getSlotTypes(true).get(CuriosWrappingUtils.curiosToAccessories(identifier));
+
+                return slot != null ? slot.icon() : SlotType.EMPTY_SLOT_ICON;
+            }
+        });
     }
 
     @SubscribeEvent
