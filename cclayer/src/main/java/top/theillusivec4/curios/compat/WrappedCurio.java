26d5a2dec1033de9251392660d409feee5a3f765
Dragon-Seeker <timekeeperguild@gmail.com>
2024-08-21 21:07:35 -0500

    Fix missing imports

--- cclayer/src/main/java/top/theillusivec4/curios/compat/WrappedCurio.java
+++ cclayer/src/main/java/top/theillusivec4/curios/compat/WrappedCurio.java
@@ -19,6 +19,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.storage.loot.LootContext;
