b4bdd8f61ed0dfe4989de502bf4254e529203a9a
Dragon-Seeker <timekeeperguild@gmail.com>
2024-08-27 15:58:01 -0500

    Make valid and invalid slot values optional fields

--- common/src/main/java/io/wispforest/accessories/api/components/AccessorySlotValidationComponent.java
+++ common/src/main/java/io/wispforest/accessories/api/components/AccessorySlotValidationComponent.java
@@ -5,14 +5,15 @@ import io.wispforest.endec.Endec;
 import io.wispforest.endec.impl.StructEndecBuilder;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public record AccessorySlotValidationComponent(Set<String> validSlotOverrides, Set<String> invalidSlotOverrides) {
     public static final AccessorySlotValidationComponent EMPTY = new AccessorySlotValidationComponent(Set.of(), Set.of());
 
     public static final Endec<AccessorySlotValidationComponent> ENDEC = StructEndecBuilder.of(
-            Endec.STRING.setOf().fieldOf("valid_slots", AccessorySlotValidationComponent::validSlotOverrides),
-            Endec.STRING.setOf().fieldOf("invalid_slots", AccessorySlotValidationComponent::invalidSlotOverrides),
+            Endec.STRING.setOf().optionalFieldOf("valid_slots", AccessorySlotValidationComponent::validSlotOverrides, Set::of),
+            Endec.STRING.setOf().optionalFieldOf("invalid_slots", AccessorySlotValidationComponent::invalidSlotOverrides, Set::of),
             AccessorySlotValidationComponent::new
     );
 
