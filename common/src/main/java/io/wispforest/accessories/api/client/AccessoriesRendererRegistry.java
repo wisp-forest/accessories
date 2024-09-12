e52de92d866650be2673504ad4594b217cc307d5
Dragon-Seeker <timekeeperguild@gmail.com>
2024-09-12 18:08:43 -0500

    Remove AccessoryRenderOverrideComponent tristate value for nullable boolean

--- common/src/main/java/io/wispforest/accessories/api/client/AccessoriesRendererRegistry.java
+++ common/src/main/java/io/wispforest/accessories/api/client/AccessoriesRendererRegistry.java
@@ -48,8 +48,8 @@ public class AccessoriesRendererRegistry {
     public static AccessoryRenderer getRender(ItemStack stack){
         var shouldOverride = stack.getOrDefault(AccessoriesDataComponents.RENDER_OVERRIDE, AccessoryRenderOverrideComponent.DEFAULT).defaultRenderOverride();
 
-        if(shouldOverride != TriState.DEFAULT) {
-            if(shouldOverride.get()) {
+        if(shouldOverride != null) {
+            if(shouldOverride) {
                 return DefaultAccessoryRenderer.INSTANCE;
             } else if(AccessoriesAPI.isDefaultAccessory(AccessoriesAPI.getOrDefaultAccessory(stack))) {
                 return null;
