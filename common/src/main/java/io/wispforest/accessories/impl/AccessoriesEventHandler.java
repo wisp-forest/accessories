522fc1e35deabfde91bb173f4788ac7d124565d8
Dragon-Seeker <timekeeperguild@gmail.com>
2024-09-15 15:20:06 -0500

    Fix issues with syncing entire container before main sync packet of slot info data

--- common/src/main/java/io/wispforest/accessories/impl/AccessoriesEventHandler.java
+++ common/src/main/java/io/wispforest/accessories/impl/AccessoriesEventHandler.java
@@ -184,9 +184,9 @@ public class AccessoriesEventHandler {
                 }
             }
         } else if (player != null) {
-            revalidatePlayer(player);
-
             networkHandler.sendToPlayer(player, syncPacket);
+
+            revalidatePlayer(player);
 
             var capability = AccessoriesCapability.get(player);
 
