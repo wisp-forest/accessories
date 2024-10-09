652e5d510a258deebf96b4672415113bb5e3047a
Dragon-Seeker <timekeeperguild@gmail.com>
2024-10-09 18:39:01 -0500

    Add EMF compat to fix issues with various Animation ResourcePacks

--- common/src/main/java/io/wispforest/accessories/mixin/client/ModelPartAccessor.java
+++ common/src/main/java/io/wispforest/accessories/mixin/client/ModelPartAccessor.java
@@ -5,8 +5,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.List;
+import java.util.Map;
 
 @Mixin(ModelPart.class)
 public interface ModelPartAccessor {
     @Accessor("cubes") List<ModelPart.Cube> getCubes();
+
+    @Accessor("children") Map<String, ModelPart> getChildren();
 }
