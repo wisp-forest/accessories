6a08d9e8c7c96850ce56fed6d17f7e08d9e07113
Dragon-Seeker <timekeeperguild@gmail.com>
2025-01-17 16:45:02 -0600

    Disable Experimental caching due to issues with desyncing data

--- common/src/main/java/io/wispforest/accessories/impl/AccessoriesHolderImpl.java
+++ common/src/main/java/io/wispforest/accessories/impl/AccessoriesHolderImpl.java
@@ -113,7 +113,12 @@ public class AccessoriesHolderImpl implements AccessoriesHolder, InstanceEndec {
 
     @Nullable
     public AccessoriesHolderLookupCache getLookupCache() {
-        return Accessories.config().useExperimentalCaching() ? this.lookupCache : null;
+        // TODO: FIX ISSUES WITH LOOK UP CACHE LEADING TO IT EITHER:
+        /*
+            - Not updating on death with YIGD
+            - Not updating with old save leading to issues where Accessorie changes not being picked up
+         */
+        return /*Accessories.config().useExperimentalCaching() ? this.lookupCache :*/ null;
     }
 
     //--
