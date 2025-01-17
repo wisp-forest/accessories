2700af2236ac34a4d2650877e6add2f21bced6f8
Dragon-Seeker <timekeeperguild@gmail.com>
2025-01-17 16:44:11 -0600

    Add missing variant of getPlayerSlots fixing #186

--- cclayer/src/main/java/top/theillusivec4/curios/api/CuriosApi.java
+++ cclayer/src/main/java/top/theillusivec4/curios/api/CuriosApi.java
@@ -29,6 +29,7 @@ import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -76,6 +77,10 @@ public final class CuriosApi {
 
   public static Map<String, ISlotType> getSlots(boolean isClient) {
     return CuriosImplMixinHooks.getSlots(isClient);
+  }
+
+  public static Map<String, ISlotType> getPlayerSlots(Player player) {
+    return CuriosApi.getPlayerSlots(player.level());
   }
 
   public static Map<String, ISlotType> getPlayerSlots(Level level) {
