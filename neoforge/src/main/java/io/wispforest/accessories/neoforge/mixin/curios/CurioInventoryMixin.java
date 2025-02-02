281822587d273c56f6b405b3ed3d0ee60f867310
Dragon-Seeker <timekeeperguild@gmail.com>
2025-02-02 15:12:40 -0600

    Add hook for CCLayer encoded curios data to prevent duplicate data when both a present after CCLayer is switch for Curios

---
+++ neoforge/src/main/java/io/wispforest/accessories/neoforge/mixin/curios/CurioInventoryMixin.java
@@ -0,0 +1,27 @@
+package io.wispforest.accessories.neoforge.mixin.curios;
+
+import net.minecraft.nbt.CompoundTag;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
+import top.theillusivec4.curios.common.capability.CurioInventory;
+
+@Pseudo
+@Mixin(value = CurioInventory.class, remap = false)
+public abstract class CurioInventoryMixin {
+    @Shadow(remap = false) CompoundTag deserialized;
+    @Shadow(remap = false) boolean markDeserialized;
+
+    // Prevent duplicate data if both Accessories and Curios is installed and the data was saved using CCLayer
+    @Inject(method = "init", at = @At(value = "HEAD"), remap = false)
+    private void accessories$preventDuplicateDataDecode(ICuriosItemHandler curiosItemHandler, CallbackInfo ci) {
+        if (this.deserialized.getBoolean("AccessoriesEncoded")) {
+            this.markDeserialized = false;
+            this.deserialized = new CompoundTag();
+        }
+    }
+}
