45348ef2ce61d1240bb4242c159e9a373bd0e33f
Dragon-Seeker <timekeeperguild@gmail.com>
2025-02-05 16:50:30 -0600

    Fix duplicate packet registration due to cherry picking

--- common/src/main/java/io/wispforest/accessories/networking/AccessoriesNetworking.java
+++ common/src/main/java/io/wispforest/accessories/networking/AccessoriesNetworking.java
@@ -48,10 +48,6 @@ public class AccessoriesNetworking {
 
         CHANNEL.registerClientboundDeferred(MenuScroll.class, MenuScroll.ENDEC);
         CHANNEL.registerClientboundDeferred(SyncOptionChange.class, SyncOptionChange.ENDEC);
-
-        CHANNEL.registerClientboundDeferred(ScreenVariantPing.class, ScreenVariantPing.ENDEC);
-
-        CHANNEL.registerClientboundDeferred(ScreenVariantPing.class, ScreenVariantPing.ENDEC);
     }
 
     @Environment(EnvType.CLIENT)
@@ -66,10 +62,6 @@ public class AccessoriesNetworking {
 
         CHANNEL.registerClientbound(MenuScroll.class, MenuScroll.ENDEC, clientHandler(MenuScroll::handlePacket));
         CHANNEL.registerClientbound(SyncOptionChange.class, SyncOptionChange.ENDEC, clientHandler(SyncOptionChange::handlePacket));
-
-        CHANNEL.registerClientbound(ScreenVariantPing.class, ScreenVariantPing.ENDEC, clientHandler(ScreenVariantPing::handlePacket));
-
-        CHANNEL.registerClientbound(ScreenVariantPing.class, ScreenVariantPing.ENDEC, clientHandler(ScreenVariantPing::handlePacket));
     }
 
     @Environment(EnvType.CLIENT)
