dc0511310a7c1090a3e778feae1c0471f4ad2844
Dragon-Seeker <timekeeperguild@gmail.com>
2025-02-28 13:47:53 -0600

    Prevent possible crash caused my modders who DONT ADD ENCHANTMENTS TO REGISTRY WHEN THEY SHOULD!

--- common/src/main/java/io/wispforest/accessories/mixin/EnchantedCountIncreaseFunctionMixin.java
+++ common/src/main/java/io/wispforest/accessories/mixin/EnchantedCountIncreaseFunctionMixin.java
@@ -21,9 +21,7 @@ public abstract class EnchantedCountIncreaseFunctionMixin {
     private int attemptAdjustCountWithAccessoriesLooting(Holder<Enchantment> holder, LivingEntity livingEntity, Operation<Integer> original, @Local(argsOnly = true) LootContext context) {
         var amount = original.call(holder, livingEntity);
 
-        var enchantments = livingEntity.registryAccess().lookupOrThrow(Registries.ENCHANTMENT);
-
-        if(enchantments.getResourceKey(holder.value()).orElseThrow().equals(Enchantments.LOOTING)){
+        if(holder.value() == context.getLevel().registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getValueOrThrow(Enchantments.LOOTING)){
             amount = ExtraEventHandler.lootingAdjustments(livingEntity, context, amount);
         }
 
