03cc2b2cd290ec6a9e77a610962a638e7f8d3884
Dragon-Seeker <timekeeperguild@gmail.com>
2025-03-21 15:34:56 -0500

    Fix LivingEntityTrinketComponent injection args not being correct for latest trinkets version

--- fabric/src/main/java/io/wispforest/accessories/fabric/mixin/trinkets/LivingEntityTrinketComponentMixin.java
+++ fabric/src/main/java/io/wispforest/accessories/fabric/mixin/trinkets/LivingEntityTrinketComponentMixin.java
@@ -3,17 +3,20 @@ package io.wispforest.accessories.fabric.mixin.trinkets;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import dev.emi.trinkets.api.LivingEntityTrinketComponent;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Pseudo
 @Mixin(value = LivingEntityTrinketComponent.class, remap = false)
 public class LivingEntityTrinketComponentMixin {
 
     @Inject(method = "readFromNbt", at = @At("HEAD"), remap = false)
-    private void stopDuplicateItemDecode(CompoundTag tag, CallbackInfo ci, @Local(argsOnly = true) LocalRef<CompoundTag> tagRef) {
+    private void stopDuplicateItemDecode(CompoundTag tag, HolderLookup.Provider lookup, CallbackInfo ci, @Local(argsOnly = true) LocalRef<CompoundTag> tagRef) {
         if (tag.contains("data_written_by_accessories")) {
             tagRef.set(new CompoundTag());
         }
