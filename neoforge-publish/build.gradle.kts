f4b678e53cb1dae55edbcc0eb8e8d520a02e5ec1
Dragon-Seeker <timekeeperguild@gmail.com>
2025-08-19 21:39:56 -0500

    Fix issues with publishing for neoforge

--- neoforge-publish/build.gradle.kts
+++ neoforge-publish/build.gradle.kts
@@ -109,12 +109,19 @@ publishing {
             artifactId = "${rootProject.property("archives_base_name")}${(if (name.isEmpty()) "" else "-${name.replace("-mojmap", "")}")}"
         }
     }
-    repositories {
-        maven {
-            url = uri(ENV["MAVEN_URL"]!!)
-            credentials {
-                username = ENV["MAVEN_USER"]
-                password = ENV["MAVEN_PASSWORD"]
+
+    var mavenUrl = ENV["MAVEN_URL"]
+    var mavenUser = ENV["MAVEN_USER"]
+    var mavenPassword = ENV["MAVEN_PASSWORD"]
+
+    if (mavenUrl != null && mavenUser != null && mavenPassword != null) {
+        repositories {
+            maven {
+                url = uri(mavenUrl)
+                credentials {
+                    username = mavenUser
+                    password = mavenPassword
+                }
             }
         }
     }
