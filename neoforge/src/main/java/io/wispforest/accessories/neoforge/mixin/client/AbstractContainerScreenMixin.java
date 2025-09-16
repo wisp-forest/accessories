25648821241d7e7aa4eb8278191a3e673b87adb4
ThatPreston <thatprestonsecure@gmail.com>
2025-09-16 07:38:28 -0500

    Fix AbstractContainerScreenMixin crashing on NeoForge

--- neoforge/src/main/java/io/wispforest/accessories/neoforge/mixin/client/AbstractContainerScreenMixin.java
+++ neoforge/src/main/java/io/wispforest/accessories/neoforge/mixin/client/AbstractContainerScreenMixin.java
@@ -3,7 +3,10 @@ package io.wispforest.accessories.neoforge.mixin.client;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.blaze3d.platform.InputConstants;
+import io.wispforest.accessories.client.AccessoriesClient;
 import io.wispforest.accessories.pond.ContainerScreenExtension;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.inventory.Slot;
@@ -31,5 +34,10 @@ public abstract class AbstractContainerScreenMixin implements ContainerScreenExt
         var override = this.isHovering_Rendering(this.hoveredSlot, mouseX, mouseY);
 
         if (override == null || override) original.call(instance, guiGraphics);
+    }
+
+    @WrapOperation(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/KeyMapping;isActiveAndMatches(Lcom/mojang/blaze3d/platform/InputConstants$Key;)Z", ordinal = 0))
+    private boolean accessories$adjustCloseCheck(KeyMapping instance, InputConstants.Key mouseKey, Operation<Boolean> original) {
+        return original.call(instance, mouseKey) || original.call(AccessoriesClient.OPEN_SCREEN, mouseKey);
     }
 }
