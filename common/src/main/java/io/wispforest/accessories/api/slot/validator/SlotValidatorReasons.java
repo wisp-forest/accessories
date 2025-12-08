3978d1285260d204f2c288d341a029ff32936b4a
Dragon-Seeker <timekeeperguild@gmail.com>
2025-12-08 16:10:59 -0600

    Update SlotValidatorReasons.java

--- common/src/main/java/io/wispforest/accessories/api/slot/validator/SlotValidatorReasons.java
+++ common/src/main/java/io/wispforest/accessories/api/slot/validator/SlotValidatorReasons.java
@@ -4,7 +4,7 @@ import io.wispforest.accessories.api.action.ActionResponse;
 import net.minecraft.network.chat.Component;
 
 public class SlotValidatorReasons {
-    public static final Component INVALID_ITEM = Component.literal("The given Item is not allow within the slot");
+    public static final Component INVALID_ITEM = Component.literal("The given Item is not allowed within the slot");
 
     static final ActionResponse ALWAYS_VALID = ActionResponse.of(false, Component.literal("Anything can fit within this slot!"));
     static final ActionResponse ALWAYS_INVALID = ActionResponse.of(false, Component.literal("The slot is unable to take any Accessory, forever!"));
